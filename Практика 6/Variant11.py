12e0ba2d1d67e908f73df454f8367b2eaff6cf74
12OxXxYmIrOn21 <112860133+12OxXxYmIrOn21@users.noreply.github.com>
2022-12-03 02:25:57 +0300

    Update Variant11.py

--- Практика 6/Variant11.py
+++ Практика 6/Variant11.py
@@ -1,8 +1,10 @@
 import re
-str = 'Нннннна тебе! Ах ты! Я тебе покажу!'
-nnn = re.findall('[Нн]+',str)
-nnn_len = map(len,nnn)
-max_n = max(nnn_len)
-str_r = str.replace('!','.')
-print(max_n)
-print(str_r)
+def F():
+  str = 'Нннннна тебе! Ах ты! Я тебе покажу!'
+  nnn = re.findall('[Нн]+',str)
+  nnn_len = map(len,nnn)
+  max_n = max(nnn_len)
+  str_r = str.replace('!','.')
+  print(max_n)
+  print(str_r)
+F()
