5f4ebcb98d3d6226f34be93a7155a97b3017746f
12OxXxYmIrOn21 <112860133+12OxXxYmIrOn21@users.noreply.github.com>
2022-12-03 02:22:45 +0300

    Update Задание7.py

--- Практика 5/Задание7.py
+++ Практика 5/Задание7.py
@@ -1,8 +1,10 @@
-a = int(input())
-b = 0
-while a != 0:
-    next = int(input())
-    if next != 0 and a < next:
-        b += 1
-    a = next
-print(b)
+def F():    
+    a = int(input())
+    b = 0
+    while a != 0:
+        next = int(input())
+        if next != 0 and a < next:
+            b += 1
+        a = next
+    print(b)
+F()
