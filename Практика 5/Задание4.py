ae254c94e0504c600a6b6c61829d3c110c0a1635
12OxXxYmIrOn21 <112860133+12OxXxYmIrOn21@users.noreply.github.com>
2022-12-03 02:17:11 +0300

    Update Задание4.py

--- Практика 5/Задание4.py
+++ Практика 5/Задание4.py
@@ -1,7 +1,7 @@
-x = int(input())
-y = int(input())
-i = 1
-while x < y:
-    x *= 1.1
-    i += 1
-print(i)
+def F(x, y):
+    i = 1
+    while x < y:
+        x *= 1.1
+        i += 1
+    print(i)
+F(6,2)
