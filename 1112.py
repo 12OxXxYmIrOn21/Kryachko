78df0c5b6a729452d89b7388ea9841c9469906aa
12OxXxYmIrOn21 <112860133+12OxXxYmIrOn21@users.noreply.github.com>
2023-05-17 09:19:08 +0300

    Delete 1112.py

--- 1112.py
+++
@@ -1,7 +0,0 @@
-x = float(input("Введите x "))
-y = float(input("Введите y "))
-z1 = float(input("Введите z1 "))
-import math
-z = z1*(10**(-4))
-s = (math.pow(2,(math.pow(y,x))))+(math.pow(math.pow(3,x),y)) - ((y*(math.asin(math.pow(1/1+z**2),0.5)))-1/3)/(math.fabs(x))+(1/((math.pow(y,2)+1)))
-print(s)
