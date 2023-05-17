007acc9a9c3b75efc8aa03fc4b5b9558c9b40d54
12OxXxYmIrOn21 <112860133+12OxXxYmIrOn21@users.noreply.github.com>
2023-05-17 09:17:54 +0300

    Delete Практика 12 directory

--- Практика 12/Блок А/Блок А(Задание4).py
+++
@@ -1,6 +0,0 @@
-def f(n,x):
-    if n == 0:
-        return x
-    else:
-        return f(n-1, (x+(n)))
-print(f(11,0))
--- Практика 12/Блок Б/Блок Б(задание1).py
+++
@@ -1,6 +0,0 @@
-def f():
-    x = int(input())
-    if x == 0:
-        return int()
-    return max([x, f()])
-print(f())
